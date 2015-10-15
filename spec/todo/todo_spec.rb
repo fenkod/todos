fcf1284b5ca429d7dda9ac84ef7abb5cde82af28
Brian Pratt <brian@8thlight.com>
2015-10-15 11:35:19 -0500

    improves login selector usage

--- spec/todo/todo_spec.rb
+++ spec/todo/todo_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 class LoginPage < SitePrism::Page
 	set_url "http://localhost:9292"
 
-	element :username_input, "input#username"
-	element :password_input, "input#password"
-	element :submit_button, "input[type='submit']"
+	element :username_input, ".login input#username"
+	element :password_input, ".login input#password"
+	element :submit_button,  ".login input[value='Login']"
 end
 
 describe "login page" do
 	let(:login) { LoginPage.new }
-	before(:each) { login.load }
+	before { login.load }
 
 	it "has a username input" do
 		expect(login).to have_username_input
@@ -19,12 +19,12 @@ describe "login page" do
 	it "has a password input" do
 		expect(login).to have_password_input
 	end
-	
+
 	it "has a submit button" do
 		expect(login).to have_submit_button
 	end
 
-	it "submits the form" do
+	it "displays a validation error" do
 		login.username_input.set("a-username")
 		login.password_input.set("a-password")
 
