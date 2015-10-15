5107b330ec527992c93b6bfdafc7bd4eb86edda5
Brian Pratt <brian@8thlight.com>
2015-10-15 11:17:09 -0500

    test failing login

--- spec/todo/todo_spec.rb
+++ spec/todo/todo_spec.rb
@@ -5,11 +5,11 @@ class LoginPage < SitePrism::Page
 
 	element :username_input, "input#username"
 	element :password_input, "input#password"
+	element :submit_button, "input[type='submit']"
 end
 
 describe "login page" do
 	let(:login) { LoginPage.new }
-
 	before(:each) { login.load }
 
 	it "has a username input" do
@@ -18,5 +18,18 @@ describe "login page" do
 
 	it "has a password input" do
 		expect(login).to have_password_input
+	end
+	
+	it "has a submit button" do
+		expect(login).to have_submit_button
+	end
+
+	it "submits the form" do
+		login.username_input.set("a-username")
+		login.password_input.set("a-password")
+
+		login.submit_button.click
+
+		expect(login).to have_content "Invalid"
 	end
 end
