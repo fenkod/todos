a464c5a395c960b7c76622d27a3c957182858cbb
Brian Pratt <brian@8thlight.com>
2015-10-15 11:11:33 -0500

    add login page element checks

--- spec/todo/todo_spec.rb
+++ spec/todo/todo_spec.rb
@@ -2,10 +2,21 @@ require 'spec_helper'
 
 class LoginPage < SitePrism::Page
 	set_url "http://localhost:9292"
+
+	element :username_input, "input#username"
+	element :password_input, "input#password"
 end
 
-describe "Sanity" do
-	it "is sane" do
-		LoginPage.new.load
+describe "login page" do
+	let(:login) { LoginPage.new }
+
+	before(:each) { login.load }
+
+	it "has a username input" do
+		expect(login).to have_username_input
+	end
+
+	it "has a password input" do
+		expect(login).to have_password_input
 	end
 end
